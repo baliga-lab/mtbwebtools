dc71446bc3e755cbe49d3b7a3ed8ffc0b22b747b
micheleenharris <micheleen.harris@systemsbiology.org>
2014-04-29 11:26:15 -0700

    got rid of flask-sqlalchemy db, just scoped_session now

--- app/__init__.py
+++ app/__init__.py
@@ -3,24 +3,20 @@ from flask.ext.sqlalchemy import SQLAlchemy
 import os
 import os.path as op
 
-from sqlalchemy import create_engine
-from sqlalchemy.orm import scoped_session, sessionmaker
-
-
 # Initialize app
 app = Flask(__name__)
 app.debug = True
 app.config.from_object('config')
 #os.environ['PYTHON_EGG_CACHE'] = '/tmp/tmp2'
-db = SQLAlchemy(app)
+#db = SQLAlchemy(app)
 
 from app.views import mod as mainModule
 app.register_blueprint(mainModule)
 
 #  For full text search
-import flask.ext.whooshalchemy as whooshalchemy
-from models import File, Gene, Condition
+#import flask.ext.whooshalchemy as whooshalchemy
+#from models import File, Gene, Condition
 
-whooshalchemy.whoosh_index(app, File)
-whooshalchemy.whoosh_index(app, Gene)
-whooshalchemy.whoosh_index(app, Condition)
+#whooshalchemy.whoosh_index(app, File)
+#whooshalchemy.whoosh_index(app, Gene)
+#whooshalchemy.whoosh_index(app, Condition)
